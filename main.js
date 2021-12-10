d3025f78b3fcc8aaf68e5b0702aa754a6674ee36
otuoio <mariocoppo91@gmail.com>
2021-12-10 14:44:44 +0100

    add dinamic post in DOM

--- main.js
+++ main.js
@@ -4,36 +4,6 @@
 //     - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
 // - Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
 
-/* <div class="post">
-    <div class="post__header">
-        <div class="post-meta">
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">Phil Mangione</div>
-                <div class="post-meta__time">4 mesi fa</div>
-            </div>
-        </div>
-    </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-    <div class="post__image">
-        <img src="https://unsplash.it/600/300?image=171" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-            </div>
-        </div>
-    </div>
-</div> */
 
 // array di oggetti
 const post = [
@@ -78,3 +48,45 @@ const post = [
         likes : 16
     },
 ];
+
+// Richiamo il container 
+const container = document.getElementById('container');
+
+// Popolo il container con gli oggetti dell'array
+for (i = 0; i < post.length; i++) {
+    const template = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="https://unsplash.it/300/300?image=${post[i].profileImage}" alt="${post[i].name}">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post[i].name}</div>
+                        <div class="post-meta__time">${post[i].date}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">${post[i].text}</div>
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?image=${post[i].image}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post[i].likes}</b> persone
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    // Inserisco il template nel DOM
+    container.innerHTML += template;
+}
